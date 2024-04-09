65bfff36ae9fb7cdeb366f7f82c07f25da4601cb
Louis-Charles Biron <louirlesbiron6@gmail.com>
2024-04-09 15:38:39 -0400

    new singeFactory() = yes2

---
+++ src/main/java/org/yes/Controller/AppController.java
@@ -0,0 +1,24 @@
+package org.yes.Controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class AppController extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("DIE");
+        stage.setAlwaysOnTop(true);
+        stage.setOpacity(0.95);
+        stage.setFullScreenExitHint("singe LOLOL");
+        stage.setScene(new Scene(new FXMLLoader(getClass().getResource("/View/index.fxml")).load(), 500, 400));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/org/yes/Controller/AppGraphicalController.java
@@ -0,0 +1,42 @@
+package org.yes.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class AppGraphicalController {
+
+    @FXML
+    private TextField test;
+
+    @FXML
+    private Label test2;
+    @FXML
+    private Label pt;
+
+   // private Mot mot = new Mot("mamburg");
+   // private Joueur singe = new Joueur();
+
+
+    @FXML
+    void clickTest(MouseEvent event) {
+        System.out.println("SINGE!!!!+sd+as+");
+
+    }
+
+    @FXML
+    private void initialize() {
+
+        test.setOnKeyPressed( event -> {
+
+        } );
+
+        test.textProperty().addListener((o, oldV, newV) -> {
+
+
+        });
+
+
+    }
+}
