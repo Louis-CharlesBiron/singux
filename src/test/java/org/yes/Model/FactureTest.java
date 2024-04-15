99625be05d0f5fa89f91c205564481813406b14f
Louis-Charles Biron <louirlesbiron6@gmail.com>
2024-04-15 15:29:13 -0400

    FACTURE TESTST AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

---
+++ src/test/java/org/yes/Model/FactureTest.java
@@ -0,0 +1,14 @@
+package org.yes.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactureTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTotalSansTaxes() {
+    }
+}
